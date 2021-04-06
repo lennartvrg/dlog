b91673e61645b1fd87b877319f15290c9ce65c47
Lennart Voorgang <lennart@voorgang.eu>
2021-04-06 23:11:26 +0200

    Added npm install step
    Signed-off-by: Lennart Voorgang <lennart@voorgang.eu>

--- nodejs/native/build.rs
+++ nodejs/native/build.rs
@@ -4,7 +4,7 @@ extern crate neon_build;
 
 fn main() {
     let out_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
-    std::fs::create_dir(std::path::Path::new(&out_dir).join("build")).unwrap();
+    std::fs::create_dir_all(std::path::Path::new(&out_dir).join("build")).unwrap();
 
     neon_build::Setup::options().output_dir("build").setup();
 }
