f1139683a12b6057de15e5e33eed02844f06fde4
Lennart Voorgang <lennart@voorgang.eu>
2021-04-17 02:32:05 +0200

    Added python publish CI CD

--- python/example/main.py
+++ python/example/main.py
@@ -5,7 +5,7 @@ from dlog_python import DlogLogger
 class Main:
     def __init__(self):
         self.logger = logging.getLogger('main')
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(DlogLogger("997a1c6f-4fff-4a7e-b399-74ac397a4fec"))
 
     def run(self):
