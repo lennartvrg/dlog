eb23f92684e95f98c1ea984a6b6263381abb47e2
Lennart Voorgang <lennart@voorgang.eu>
2021-04-22 02:31:36 +0200

    Moved min logging level to debug
    Signed-off-by: Lennart Voorgang <lennart@voorgang.eu>

--- dlog_rs/src/lib.rs
+++ dlog_rs/src/lib.rs
@@ -14,5 +14,5 @@ pub fn configure(api_key: impl Into<String>) {
         panic!("[dlog] Failed to configure dlog: {}", err)
     }
 
-    log::set_max_level(LevelFilter::Info);
+    log::set_max_level(LevelFilter::Debug);
 }
