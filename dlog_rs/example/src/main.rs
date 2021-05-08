591c90aeea50c5f0a57f384703afaf83caee1d61
Lennart <7083261+lennartvrg@users.noreply.github.com>
2021-05-08 07:40:00 +0200

    Fixed stack overflow (#19)

---
+++ dlog_rs/example/src/main.rs
@@ -0,0 +1,10 @@
+use log::Level;
+
+fn main() {
+    dlog_rs::Builder::new()
+        .with_env_api_key("DLOG_API_KEY")
+        .with_level(Level::Info)
+        .build();
+
+    log::info!("Hello, world!");
+}
